249887ad8890ff48a1bc94334fdc2f69246d5682
M Imran Ghafoor <110387290+Imran-Ghafoor@users.noreply.github.com>
2024-03-01 06:24:30 +0500

    add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
+import { useState } from "react";
 import Head from "./components/Head";
 import Task from "./components/Task";
+import React from "react";
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      title: "Im done with react",
+    },
+    {
+      id: 2,
+      title: "I know about react",
+    },
+    {
+      id: 3,
+      title: "Do you like tailwind",
+    },
+  ];
+  const [list, setList] = useState(data);
+
+  let newEle = {
+    id: 5,
+    title: "ok ok this one is added",
+  };
+
+  const addEleIntoList = () => {
+    let newArr = [...list, newEle];
+    setList(newArr);
+  };
+  const deleteEle = (x) => {
+    setList(list.filter((e) => e.id !== x));
+  };
   return (
-    <>
-      <Head tittle={"ToDo App With Hadi"} para={"Hi, Im ToDo Let's Fun With me!"} />
-      <Task/>
-      <Task/>
-      <Task/>
-      <Task/>
-    </>
+    <div>
+      <Head
+        tittle={"ToDo App with Hadi"}
+        para={"Hi, Im ToDo Let's Fun With me!"}
+      />
+      <button
+        onClick={addEleIntoList}
+        className="flex flex-col justify-center items-center text-center text-white "
+      >
+        Add Task
+      </button>
+      {list.map((x, index) => (
+        <React.Fragment key={index}>
+          <Task title={x.title} removeEle={() => deleteEle(x.id)} />
+        </React.Fragment>
+      ))}
+    </div>
   );
 }
 
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import { FaSkullCrossbones } from "react-icons/fa6";
 import { FaUserCheck } from "react-icons/fa";
 
-const Task = () => {
+const Task = ({ title, removeEle }) => {
   return (
-    <div className="flex  justify-center">
-      <div className="min-w-[450px] flex justify-between mt-16 text-white bg-green-500 p-3 rounded-lg">
-        <h2 className=" text-2xl ">Enter the new task</h2>
+    <div className="flex  justify-center text-blue-900 font-semibold">
+      <div className="min-w-[450px] flex justify-between mt-10 bg-green-500 p-3 rounded-lg">
+        <h2 className=" text-2xl ">{title}</h2>
 
         <div className="flex flex-row items-center space-x-5">
-          <span className="flex justify-end">
-            <FaSkullCrossbones size={25} />
+          <span onClick={removeEle} className="flex justify-end">
+            <FaSkullCrossbones size={25} color="white" />
           </span>
           <span>
-            <FaUserCheck size={25} />
+            <FaUserCheck size={25} color="white" />
           </span>
         </div>
       </div>
